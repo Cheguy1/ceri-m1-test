a63cd4df576968fd4a3f756a2a84960d472c2c23
Florian DEMOL <floriandemol@MacBook-Pro-de-Florian.local>
2017-05-05 10:55:43 +0200

    commit 8

--- src/test/java/fr/univavignon/pokedex/impl/PokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/impl/PokedexTest.java
@@ -3,28 +3,22 @@ package fr.univavignon.pokedex.impl;
 import fr.univavignon.pokedex.api.IPokedexTest;
 import fr.univavignon.pokedex.api.IPokemonFactory;
 import fr.univavignon.pokedex.api.IPokemonMetadataProvider;
-import fr.univavignon.pokedex.api.PokedexException;
-import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
 import org.mockito.Mock;
-
-import java.io.File;
-
-import static junit.framework.TestCase.assertEquals;
 
 
 public class PokedexTest extends IPokedexTest {
 	
 	@Mock
-	IPokemonMetadataProvider metadataProviderMock;
+	IPokemonMetadataProvider ipokemetaprovider;
 	@Mock
-	IPokemonFactory pokeFactory;
+	IPokemonFactory ipokeFacMock;
 	
-    @Before
+    @Override
+	@Before
     public void setUp() {
-    	ipokedexMock = new Pokedex(metadataProviderMock, pokeFactory);
-    	ipokedexMockAdd = new Pokedex(metadataProviderMock, pokeFactory);
+    	ipokedexMock = new Pokedex(ipokemetaprovider, ipokeFacMock);
+    	ipokedexMockAdd = new Pokedex(ipokemetaprovider, ipokeFacMock);
     }
 
 }
