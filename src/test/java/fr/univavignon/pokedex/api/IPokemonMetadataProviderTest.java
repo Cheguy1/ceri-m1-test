39becf95f347bd3cdebbc88400d2f9252b18830e
Florian DEMOL <floriandemol@MacBook-Pro-de-Florian.local>
2017-05-05 09:53:35 +0200

    commit 3

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,7 +1,6 @@
 package fr.univavignon.pokedex.api;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 
 import org.junit.Before;
@@ -16,38 +15,44 @@ import org.mockito.runners.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class IPokemonMetadataProviderTest {
 	
-	static PokemonMetadata PokemonMeta;
-	
-	@Mock
-	IPokemonMetadataProvider ipokemetaprovider;
-	
 	@Rule
 	public MockitoRule mockitoRule = MockitoJUnit.rule();
 	
+	protected static PokemonMetadata pokemeta;
+	
+	@Mock
+	private IPokemonMetadataProvider ipokemetaprovider;
 	
 	@Before
 	public void setUp() {
-		PokemonMeta = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
+		pokemeta = new PokemonMetadata(0,"Bulbizarre",126,126,90);
 		configure(ipokemetaprovider);
 
 	}
 	
-    @Test
-    public void testGetPokeMetadata() throws PokedexException {
-        PokemonMetadata p = ipokemetaprovider.getPokemonMetadata(25);
-        
-        assertTrue(p != null);
-		assertEquals(p.getName(), "Bulbizarre");
-		assertEquals(p.getAttack(), 126);
-		assertEquals(p.getDefense(), 126);
-		assertEquals(p.getStamina(), 90);
-        
-    }
-	
 	public static void configure(IPokemonMetadataProvider i) {
 		try {
-			when(i.getPokemonMetadata(0)).thenReturn(new PokemonMetadata(0, "Bulbizarre", 126, 126, 90));
+			when(i.getPokemonMetadata(123)).thenReturn(new PokemonMetadata(0,"Bulbizarre",126,126,90));
 		} catch (PokedexException e) {e.printStackTrace();}
 	}
-
+	
+	
+	@Test
+	public void test() {
+		
+		PokemonMetadata pm = new PokemonMetadata(0,"Bulbizarre",126,126,90);
+		int index = 0;
+		assertNotNull(ipokemetaprovider);
+		try {
+			assertEquals(pm.getIndex(), ipokemetaprovider.getPokemonMetadata(index).getIndex());
+			assertEquals(pm.getName(), ipokemetaprovider.getPokemonMetadata(index).getName());
+			assertEquals(pm.getAttack(), ipokemetaprovider.getPokemonMetadata(index).getAttack());
+			assertEquals(pm.getDefense(), ipokemetaprovider.getPokemonMetadata(index).getDefense());
+			assertEquals(pm.getStamina(), ipokemetaprovider.getPokemonMetadata(index).getStamina());
+		} catch (PokedexException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
 }
