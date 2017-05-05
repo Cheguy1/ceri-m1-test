0534c76e2c753ba5be4002457f98810311b5d351
Florian DEMOL <floriandemol@MacBook-Pro-de-Florian.local>
2017-05-05 11:42:01 +0200

    commit 12

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -22,7 +22,7 @@ public class IPokemonFactoryTest {
 
     @Before
     public void setUp() throws PokedexException {
-        when(ipokeFacMock.createPokemon(1, 613, 64, 4000, 4)).thenReturn(new Pokemon(1, "bulbasaur", 49, 49, 90, 613, 64, 4000, 4, 56));
+        when(ipokeFacMock.createPokemon(1, 613, 64, 4000, 4)).thenReturn(new Pokemon(1, "bulbasaur", 49, 49, 0, 613, 64, 4000, 4, 54));
     }
 
     @Test
@@ -32,11 +32,11 @@ public class IPokemonFactoryTest {
         assertEquals("bulbasaur", pokemon.getName());
         assertEquals(49, pokemon.getAttack());
         assertEquals(49, pokemon.getDefense());
-        assertEquals(90, pokemon.getStamina());
+        assertEquals(0, pokemon.getStamina());
         assertEquals(613, pokemon.getCp());
         assertEquals(64, pokemon.getHp());
         assertEquals(4000, pokemon.getDust());
         assertEquals(4, pokemon.getCandy());
-        assertEquals(56.0, pokemon.getIv());
+        assertEquals(54.0, pokemon.getIv());
     }
 }
